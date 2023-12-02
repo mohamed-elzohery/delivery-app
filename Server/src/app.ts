b1df2efc6546c276c3db23dbfedd383c2594dba2
Mohamed Elzohery <m.zohery1998@gmail.com>
2023-12-02 16:33:05 +0200

    adding main app config

---
+++ Server/src/app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import path from "path";
+
+const app = express();
+process.env.NODE_ENV !== "production" && app.use(morgan("dev"));
+
+dotenv.config({
+  path: path.resolve(__dirname + `/config/${process.env.NODE_ENV}.env`),
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.all("*", (req, res) =>
+  res.status(404).json({ message: "Undefinded Routes" })
+);
+
+export default app;
